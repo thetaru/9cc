f27833ab38bec9812dade3dedd02c791e9d44db7
thetaru <sensesensesense012345678910@gmail.com>
2021-08-22 08:15:51 +0000

    単項演算子+/-を追加

--- 9cc.c
+++ 9cc.c
@@ -163,6 +163,7 @@ Node *new_node_num(int val) {
 // Proto
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 // expr = mul ("+" mul | "-" mul)"
@@ -179,18 +180,31 @@ Node *expr() {
 	}
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-	Node *node = primary();
+	Node *node = unary();
 	
 	for (;;) {
 		if (consume('*'))
-			node = new_node(ND_MUL, node, primary());
+			node = new_node(ND_MUL, node, unary());
 		else if (consume('/'))
-			node = new_node(ND_DIV, node, primary());
+			node = new_node(ND_DIV, node, unary());
 		else
 			return node;
 	}
+}
+
+// unary = ("+" | "-")? primary | primary
+Node *unary() {
+	if (consume('+'))
+		// +x
+		// -> x
+		return unary();
+	if (consume('-'))
+		// -x
+		// -> 0-x
+		return new_node(ND_SUB, new_node_num(0), unary());
+	return primary();
 }
 
 // primary = "(" expr ")" | num
