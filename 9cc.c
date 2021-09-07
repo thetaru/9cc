419633908a489acdde80ef4ac75b925787d502fc
thetaru <sensesensesense012345678910@gmail.com>
2021-09-07 18:17:37 +0000

    テストの修正(Cで記述したものに差し替え)

--- 9cc.c
+++ 9cc.c
@@ -8,9 +8,8 @@ int main(int argc, char *argv[]) {
 
 	// トークナイズしてパースする
 	// 結果はcodeに保存する
-	//filename = argv[1];
-	//user_input = read_file(filename);
-	user_input = argv[1];
+	filename = argv[1];
+	user_input = read_file(filename);
 	token = tokenize();
 	program();
 
--- test.sh
+++ test.sh
@@ -11,98 +11,10 @@ int fib(int n) { if(n==1) return 1; if(n==2) return 1; return fib(n-1) + fib(n-2
 void alloc4(int **p, int a, int b, int c, int d) { *p = malloc(sizeof(int) * 4); (*p)[0] = a; (*p)[1] = b; (*p)[2] = c; (*p)[3] = d; }
 EOF
 
-assert() {
-    expected="$1"
-    input="$2"
-
-    ./9cc "$input" > tmp.s
+run() {
+    ./9cc ./tests  > tmp.s
     cc -static -o tmp tmp.s tmp2.o
     ./tmp
-    actual="$?"
-
-    if [ "$actual" = "$expected" ]; then
-        echo "$input => $actual"
-    else
-        echo "$input => $expected expected, but got $actual"
-        exit 1
-    fi
-    echo ""
 }
 
-assert 1 "int a; int main() { a = 1; return a; }"
-assert 1 "int main() { return 1; }"
-
-assert 6 "int main() { int a; a = 2; int b; b = 3; return a * b; }"
-
-assert 2 "int main() { int a; a = 0 < 1; int b; b = 0 <= 1; int c; c = 0 > 1; int d; d = 0 >= 1; return a + b + c + d; }"
-
-assert 0 "int main() { int a; a = 0; if (a == 0) { return 0; } }"
-
-assert 1 "int main() { int a; a = 0; if (a != 0) { return 0; } else { return 1; } }"
-
-assert 10 "int main() { int i; i = 1; while (i < 10) { i = i + 1; } return i; }"
-
-assert 55 "int main() { int sum; sum = 0; int i; i = 1; for (i = 1; i <= 10; i = i + 1) sum = sum + i; return sum; }"
-
-assert 10 "int main() { for (;;) { return 10; } }"
-
-assert 1 "int main() { return test1(); }"
-assert 2 "int main() { return test2(); }"
-assert 3 "int main() { return test3(1, 2); }"
-assert 4 "int main() { return test4(5, 1); }"
-assert 5 "int main() { return fib(5); }"
-assert 5 "int main() { return const(); } int const() { return 5; }"
-assert 6 "int main() { int a; a = 2; int b; b = 3; return a * b; }"
-assert 3 "int main() { return id(3); } int id(int x) { return 3; }"
-assert 3 "int main() { int a; a = 3; return id(a); } int id(int a) { return a; }"
-assert 5 "int main() { int a; a = 3; return id(a); } int id(int a) { a = 5; return a; }"
-assert 3 "int main() { int a; a = 3; return id(a); } int id(int x) { return x; }"
-assert 5 "int main() { int a; a = 5; return fibo(a); } int fibo(int n) { if(n==1) return 1; if(n==2) return 1; return fib(n-1) + fib(n-2); }"
-
-assert 3 "int main() { int x; x = 3; int y; y = &x; return *y; }"
-assert 3 "int main() { int x; x = 3; int y; y = 5; int z; z = &y + 8; return *z; }"
-
-assert 3 "int main() { int x; int *y; y = &x; *y = 3; return x; }"
-
-assert 4 "int main() { int *p; alloc4(&p, 1, 2, 4, 8); int *q; q = p + 2; return *q; }"
-assert 8 "int main() { int *p; alloc4(&p, 1, 2, 4, 8); int *q; q = p + 2; q = p + 3; return *q; }"
-
-assert 4 "int main() { int x; return sizeof(x); }"
-assert 8 "int main() { int *x; return sizeof(x); }"
-assert 4 "int main() { int *x; return sizeof(1); }"
-assert 4 "int main() { int *x; return sizeof(sizeof(1)); }"
-assert 4 "int main() { int *x; return sizeof(*x); }"
-assert 8 "int main() { int *x; return sizeof(x+1); }"
-assert 8 "int main() { int *x; return sizeof(1+x); }"
-assert 8 "int main() { int *x; int y; y = 1; return sizeof(x+y); }"
-#assert 8 "int main() { int *x; int y; y = 1; return sizeof(y+x); }"
-assert 8 "int main() { int **x; return sizeof(*x+1); }"
-assert 4 "int main() { int **x; return sizeof(**x+1); }"
-
-assert 0 "int main() { int a[5]; return 0; }"
-
-assert 3 "int main() { int a[2]; *a = 1; *(a + 1) = 2; int *p; p = a; return *p + *(p + 1); }"
-
-assert 3 "int main() { int a[2]; a[0] = 1; a[1] = 2; int *p; p = a; return p[0] + p[1]; }"
-assert 3 "int main() { int a[2]; a[0] = 1; a[1] = 2; int *p; p = a; return p[0] + p[1]; }"
-
-assert 0 "int a; int b[2]; int main() { return 0; }"
-assert 0 "int a; int b[2]; int main() { int x; return 0; }"
-assert 1 "int a; int main() { a = 1; return a; }"
-assert 2 "int a; int main() { int a; a = 2; return a; }"
-assert 5 "int a; int main() { a = 2; int b; b = 3; return a + b; }"
-
-assert 1 "int main() { char a; return sizeof(a); }"
-assert 3 "int main() { char x[3]; x[0] = -1; x[1] = 2; int y; y = 4; return x[0] + y; }"
-
-assert 0 'int main() { char *a; a = "abc"; return 0; }'
-assert 0 'int main() { char *a; a = "abcdefghijklmnopqrstuvwxyz"; return 0; }'
-assert 97 'int main() { char *a; a = "a"; printf(a); return a[0]; }'
-
-assert 0 "int main() { /* NekoCat InuDog */ return 0; }"
-assert 0 "int main() {
-  // SaruMonkey ToriBird
-  return 0;
-}"
-
-echo OK
+run
